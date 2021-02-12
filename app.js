7a9c1233767d69bbd7a1f02af4252a9b8dc69adc
Cyril <sahayacyril2015@gmail.com>
2021-02-12 22:35:14 +0530

    Created compose page

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res) {
   res.render("home", {StartingContent : homeStartingContent});
 });
@@ -31,6 +33,15 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+
+  res.redirect("/");
+});
 
 
 
