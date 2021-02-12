f7fdabc7f6e11eaf58fdf78e2265827aa658429d
Cyril <sahayacyril2015@gmail.com>
2021-02-12 23:04:39 +0530

    Feature: Add post blog

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.use(express.static("public"));
 let posts = [];
 
 app.get("/", function(req, res) {
-  res.render("home", {StartingContent : homeStartingContent});
+  res.render("home", {
+    StartingContent : homeStartingContent, 
+    posts: posts
+  });
 });
 
 app.get("/about", function(req, res) {
